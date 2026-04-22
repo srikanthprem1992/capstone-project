7635280b86bafde916b25a752abb6608e78f1818
Srikanth Prem (Omnicom) <srikanth.prem@omc.com>
2026-04-23 01:09:13 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
 
+# app.db file creation
+Base.metadata.create_all(bind=engine)
+
+#fast api 
 app = FastAPI(title="Order Management System")
 
 @app.get("/")
